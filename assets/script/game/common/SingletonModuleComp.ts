436b82c2d0beba2f480fe4ad007f1b09a6b90b24
dgflash <dgflash@qq.com>
2022-08-08 09:57:46 +0800

    .

--- assets/script/game/common/SingletonModuleComp.ts
+++ assets/script/game/common/SingletonModuleComp.ts
@@ -2,11 +2,11 @@
  * @Author: dgflash
  * @Date: 2021-11-18 14:20:46
  * @LastEditors: dgflash
- * @LastEditTime: 2022-08-03 10:07:06
+ * @LastEditTime: 2022-08-08 09:55:52
  */
 
-import { ecs } from "../../../../../extensions/oops-plugin-framework/assets/libs/ecs/ECS";
-import { Initialize } from "../../initialize/Initialize";
+import { ecs } from "../../../../extensions/oops-plugin-framework/assets/libs/ecs/ECS";
+import { Initialize } from "../initialize/Initialize";
 
 /** 游戏模块 */
 @ecs.register('SingletonModule')
