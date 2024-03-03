e9d7dd263a4413b981b5170180efa78729d82f70
dgflash <dgflash@qq.com>
2024-03-03 13:58:05 +0800

    新版界面模板对象API演示程序

--- assets/script/game/account/view/DemoViewComp.ts
+++ assets/script/game/account/view/DemoViewComp.ts
@@ -1,4 +1,5 @@
-import { _decorator } from "cc";
+import { EventKeyboard, EventTouch, _decorator } from "cc";
+import { oops } from "../../../../../extensions/oops-plugin-framework/assets/core/Oops";
 import { ecs } from "../../../../../extensions/oops-plugin-framework/assets/libs/ecs/ECS";
 import { CCComp } from "../../../../../extensions/oops-plugin-framework/assets/module/common/CCComp";
 
@@ -10,17 +11,45 @@ const { ccclass, property } = _decorator;
 export class DemoViewComp extends CCComp {
     /** 视图层逻辑代码分离演示 */
     start() {
-        // var entity = this.ent as ecs.Entity;         // ecs.Entity 可转为当前模块的具体实体对象
-        // this.on(ModuleEvent.Cmd, this.onHandler, this);
+        this.setButton("onBtnTopLeft", "onBtnTopRight", "onBtnGlobalEvent");
+        this.setEvent("onGlobal");
+        this.setKeyboard(true);
+        this.setGameShow();
+        this.setGameHide();
+        this.setGameResize();
     }
 
-    /** 全局消息逻辑处理 */
-    // private onHandler(event: string, args: any) {
-    //     switch (event) {
-    //         case ModuleEvent.Cmd:
-    //             break;
-    //     }
-    // }
+    private onBtnTopLeft(event: EventTouch) {
+        oops.gui.toast("点击左上按钮");
+    }
+
+    private onBtnTopRight(event: EventTouch) {
+        oops.gui.toast("点击右上按钮");
+    }
+
+    private onBtnGlobalEvent(event: EventTouch) {
+        this.dispatchEvent("onGlobal", "收到全局事件");
+    }
+
+    private onGlobal(event: string, args: any) {
+        oops.gui.toast(args);
+    };
+
+    protected onKeyDown(event: EventKeyboard): void {
+        oops.gui.toast(`按键事件的按键码为：${event.keyCode}`);
+    }
+
+    protected onGameShow(): void {
+        console.log("监听游戏从后台进入事件");
+    }
+
+    protected onGameHide(): void {
+        console.log("监听游戏切到后台事件");
+    }
+
+    protected onGameResize() {
+        console.log("游戏尺寸变化");
+    }
 
     /** 视图对象通过 ecs.Entity.remove(ModuleViewComp) 删除组件是触发组件处理自定义释放逻辑 */
     reset() {
