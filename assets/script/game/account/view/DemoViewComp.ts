2cc84101f3503a3e8070af24a4d2ee2d2156092c
dgflash <dgflash@qq.com>
2024-03-10 11:55:47 +0800

    .

--- assets/script/game/account/view/DemoViewComp.ts
+++ assets/script/game/account/view/DemoViewComp.ts
@@ -8,21 +8,10 @@ const { ccclass, property } = _decorator;
 @ccclass('DemoViewComp')
 @ecs.register('DemoView', false)
 export class DemoViewComp extends CCComp {
-    /** 视图层逻辑代码分离演示 */
     start() {
-        // var entity = this.ent as ecs.Entity;         // ecs.Entity 可转为当前模块的具体实体对象
-        // this.on(ModuleEvent.Cmd, this.onHandler, this);
+      
     }
 
-    /** 全局消息逻辑处理 */
-    // private onHandler(event: string, args: any) {
-    //     switch (event) {
-    //         case ModuleEvent.Cmd:
-    //             break;
-    //     }
-    // }
-
-    /** 视图对象通过 ecs.Entity.remove(ModuleViewComp) 删除组件是触发组件处理自定义释放逻辑 */
     reset() {
         this.node.destroy();
     }
